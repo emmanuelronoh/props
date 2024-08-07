c64013acc572b9848a28c9686d8b6817f0ea0943
emmanuel ronoh <eronoh036@gmail.com>
2024-08-07 13:20:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// const title = document.getElementById("title");
+// title.textContent = "testing";
+
+function greetings (name) {
+    console.log(`Hello ${name}`);
+}
+
+greetings("stella");
+greetings("emmanuel");
